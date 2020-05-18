bc1de63bb2b9c9f09910f234eea4a1404da1ecf0
Pavan Chaggar <pavanchaggar@pavans-mbp.lan>
2020-05-18 17:46:57 +0100

    Adding requirements for Connectome object

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setup(
     description='A toolbox for mapping brain image analysis to a connectome',
     license='Apache License',
     maintainer='Pavan Chaggar, Travis Thompson, Alain Goriely',
-    maintainer_email='pavanjit.chaggar@maths.ox.ac.uk', 'travis.thompson@maths.ox.ac.uk', 'goriely@maths.ox.ac.uk'
+    maintainer_email='pavanjit.chaggar@maths.ox.ac.uk; travis.thompson@maths.ox.ac.uk; goriely@maths.ox.ac.uk',
     include_package_data=True,
     packages = find_packages(include=('Main', 'Main.*')),
     install_requires=[
-
-    ]
+        'nibabel',
+        'numpy',
+        'pandas'
+    ],
+)
