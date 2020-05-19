826674ef8cff32ed02f3e7f067475b4329ce7812
Pavan Chaggar <pavanchaggar@pavans-mbp.lan>
2020-05-19 17:23:44 +0100

    Demo.py shows connectome loading and plotting

--- Demo/demo.py
+++ Demo/demo.py
@@ -2,24 +2,22 @@
 """
 import os
 import nibabel as nib
+from nilearn import plotting
 from Main.Connectome import Connectome
 
-root_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.split(os.path.dirname(__file__))[0]
 
 #path to graphml
-path_to_graphml = root_dir + 'images/master_graphs_highres/33-master/master-std33-highres.graphml'
+path_to_graphml = root_dir + '/Data/master_graphs_highres/33-master/master-std33-highres.graphml'
 
 #read_graphml
-Connectome = Connectome(path_to_graphml)
+Connectome = Connectome(filename = path_to_graphml)
 
 #path to MNI parcellation
-path_to_mni_parcellation = root_dir + '../Data/mni_parcellations/mni_template-L2018_desc-scale1_atlas.nii.gz'
 
-#load mni_parcellation image
-mni_parcellation = nib.load(path_to_mni_parcellation)
+Connectome.get_nodes_graphml()
+Connectome.define_adjacency_matrix_graphml()
+Connectome.get_mni_node_coordinates()
 
-#set connectome coordinates according to mni parcellation
-Connectome.get_and_set_coordinates(path_to_mni_parcellation)
-
-#Load Data from Mindboggle/SPM
-#Mindboggle
+plotting.plot_connectome(node_coords=Connectome.coordinates, adjacency_matrix=Connectome.adj_matrix_full)
+plotting.show()
