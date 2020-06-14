1c22cac771943551b897b22e527aeac37531a3e2
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-06-14 16:10:24 +0100

    Updating script to add two groups. Two groups are needed for Dartel.

--- Main/spm_scripts/spm_smri_preprocessing.m
+++ Main/spm_scripts/spm_smri_preprocessing.m
@@ -10,19 +10,23 @@
 %% Create Subject List
 
 % path to ADNI directory
-data_dir = '/Users/pavanchaggar/Documents/ADNI/';
+data_dir = '/home/sabs-r3/Documents/atrophy_analysis/Data/LMCI_MRI/';
 
 % path to ADNI csv file containing subject information
-csv_path = '/Users/pavanchaggar/Documents/ADNI/MRI_6_01_2020.csv';
+group1_csv_path = '/home/sabs-r3/Documents/atrophy_analysis/Data/LMCI_MRI/LMCI_MRImatched_ABTAUPET_6_14_2020.csv';
+group1_csv = readtable(group1_csv_path);
+
+group2_csv_path = '/home/sabs-r3/Documents/atrophy_analysis/Data/LMCI_MRI/CN_PETmatched_MRI_6_14_2020.csv';
+group2_csv = readtable(group2_csv_path);
+
+all_csv = [group1_csv; group2_csv];
 
 % output path for tissue volumes
-tissue_vol_output = '/Users/pavanchaggar/Documents/ADNI/tissue_vols.csv';
+tissue_vol_output = '/home/sabs-r3/Documents/atrophy_analysis/Data/LMCI_MRI/tissue_vols.csv';
 
-csv = readtable(csv_path);
+subject_ids = all_csv.('Subject');
 
-subject_ids = csv.('Subject');
-
-subject_groups = csv.('Group');
+subject_groups = all_csv.('Group');
 
 % list containing paths to subject nii images
 subjects = strcat(data_dir, subject_groups, '_', subject_ids, '/', subject_ids, '.nii');
@@ -33,27 +37,27 @@ matlabbatch{1}.spm.spatial.preproc.channel.vols = subjects;
 matlabbatch{1}.spm.spatial.preproc.channel.biasreg = 0.001;
 matlabbatch{1}.spm.spatial.preproc.channel.biasfwhm = 60;
 matlabbatch{1}.spm.spatial.preproc.channel.write = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'/Volumes/Pavan_SSD/matlab_pkgs/spm12/tpm/TPM.nii,1'};
+matlabbatch{1}.spm.spatial.preproc.tissue(1).tpm = {'/home/sabs-r3/Documents/MATLAB/spm12/tpm/TPM.nii,1'};
 matlabbatch{1}.spm.spatial.preproc.tissue(1).ngaus = 1;
 matlabbatch{1}.spm.spatial.preproc.tissue(1).native = [1 1];
 matlabbatch{1}.spm.spatial.preproc.tissue(1).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {'/Volumes/Pavan_SSD/matlab_pkgs/spm12/tpm/TPM.nii,2'};
+matlabbatch{1}.spm.spatial.preproc.tissue(2).tpm = {'/home/sabs-r3/Documents/MATLAB/spm12/tpm/TPM.nii,2'};
 matlabbatch{1}.spm.spatial.preproc.tissue(2).ngaus = 1;
 matlabbatch{1}.spm.spatial.preproc.tissue(2).native = [1 1];
 matlabbatch{1}.spm.spatial.preproc.tissue(2).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {'/Volumes/Pavan_SSD/matlab_pkgs/spm12/tpm/TPM.nii,3'};
+matlabbatch{1}.spm.spatial.preproc.tissue(3).tpm = {'/home/sabs-r3/Documents/MATLAB/spm12/tpm/TPM.nii,3'};
 matlabbatch{1}.spm.spatial.preproc.tissue(3).ngaus = 2;
 matlabbatch{1}.spm.spatial.preproc.tissue(3).native = [1 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(3).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {'/Volumes/Pavan_SSD/matlab_pkgs/spm12/tpm/TPM.nii,4'};
+matlabbatch{1}.spm.spatial.preproc.tissue(4).tpm = {'/home/sabs-r3/Documents/MATLAB/spm12/tpm/TPM.nii,4'};
 matlabbatch{1}.spm.spatial.preproc.tissue(4).ngaus = 3;
 matlabbatch{1}.spm.spatial.preproc.tissue(4).native = [0 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(4).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {'/Volumes/Pavan_SSD/matlab_pkgs/spm12/tpm/TPM.nii,5'};
+matlabbatch{1}.spm.spatial.preproc.tissue(5).tpm = {'/home/sabs-r3/Documents/MATLAB/spm12/tpm/TPM.nii,5'};
 matlabbatch{1}.spm.spatial.preproc.tissue(5).ngaus = 4;
 matlabbatch{1}.spm.spatial.preproc.tissue(5).native = [0 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(5).warped = [0 0];
-matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {'/Volumes/Pavan_SSD/matlab_pkgs/spm12/tpm/TPM.nii,6'};
+matlabbatch{1}.spm.spatial.preproc.tissue(6).tpm = {'/home/sabs-r3/Documents/MATLAB/spm12/tpm/TPM.nii,6'};
 matlabbatch{1}.spm.spatial.preproc.tissue(6).ngaus = 2;
 matlabbatch{1}.spm.spatial.preproc.tissue(6).native = [0 0];
 matlabbatch{1}.spm.spatial.preproc.tissue(6).warped = [0 0];
@@ -112,7 +116,7 @@ matlabbatch{3}.spm.tools.dartel.mni_norm.fwhm = [8 8 8];
 % Compute tissue vols
 matlabbatch{4}.spm.util.tvol.matfiles(1) = cfg_dep('Segment: Seg Params', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','param', '()',{':'}));
 matlabbatch{4}.spm.util.tvol.tmax = 3;
-matlabbatch{4}.spm.util.tvol.mask = {'/Users/pavanchaggar/Documents/matlab_pkgs/spm12/tpm/mask_ICV.nii,1'};
+matlabbatch{4}.spm.util.tvol.mask = {'/home/sabs-r3/Documents/MATLAB/spm12/tpm/mask_ICV.nii,1'};
 matlabbatch{4}.spm.util.tvol.outf = tissue_vol_output;
 
 spm_jobman('run',matlabbatch)
