d82e7502f13f356e4e91d8fc1dbcb471a4a0d9c6
Pavan Chaggar <pavanchaggar@pavans-mbp.home>
2020-06-16 17:23:44 +0100

    Wrong script uploaded. Deleting.

--- Main/spm_scripts/spm_smri_jacobiandeterminants.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/home/sabs-r3/Documents/atrophy_analysis/Development/mbmgroup-refs/Main/spm_scripts/spm_smri_jacobiandeterminants_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'PET');
-spm_jobman('run', jobs, inputs{:});
