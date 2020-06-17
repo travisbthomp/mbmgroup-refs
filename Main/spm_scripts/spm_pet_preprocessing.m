579eac9893e83c227f23bbab07d0353a02910811
pavanchaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-06-17 18:26:30 +0100

    Changing function from normalisation to registration with compliment sMRI

--- Main/spm_scripts/spm_pet_preprocessing.m
+++ Main/spm_scripts/spm_pet_preprocessing.m
@@ -3,38 +3,45 @@
 % 1. Registration to mni space
 %-----------------------------------------------------------------------
 %-----------------------------------------------------------------------
-
+clear all 
 % path to ADNI directory
-data_dir = '/Users/pavanchaggar/Documents/PET/';
+data_dir = '/home/sabs-r3/Documents/AD/CN/';
 
 % path to ADNI csv file containing subject information
-csv_path = '/Users/pavanchaggar/Documents/PET/AD_CN_PET_TEST_6_02_2020.csv';
+pet_csv_path = strcat(data_dir, 'PET/CN_MRImatched_PET_6_16_2020.csv');
 
 % output path for tissue volumes
-csv = readtable(csv_path);
+pet_csv = readtable(pet_csv_path);
 
-subject_ids = csv.('Subject');
+subject_ids = pet_csv.('Subject');
 
-subject_groups = csv.('Group');
+pet_group = pet_csv.('Group');
+
+%MRI path 
+mri_csv_path = strcat(data_dir, 'MRI/CN_PETmatched_MRI_6_14_2020.csv');
+
+% output path for tissue volumes
+mri_csv = readtable(mri_csv_path);
+
+mri_group = pet_csv.('Group');
 
 % list containing paths to subject nii images
-subjects = strcat(data_dir, subject_groups, '_', subject_ids, '/', subject_ids, '.nii');
+mri_image = strcat(data_dir, 'MRI/', mri_group, '_', subject_ids, '/', subject_ids, '.nii');
+pet_image = strcat(data_dir, 'PET/', pet_group, '_', subject_ids, '/', subject_ids, '.nii');
 
-for i = 1:length(subjects)
-matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = subjects(i);
-matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = subjects(i);
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'/Users/pavanchaggar/Documents/matlab_pkgs/spm12/tpm/TPM.nii'};
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
-matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
-matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
-                                                             78 76 85];
-matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [1.5 1.5 1.5];
-matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
-matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.prefix = 'norm';
-
+for i = 1:length(subject_ids)
+matlabbatch{1}.spm.spatial.coreg.estwrite.ref = mri_image(i);
+matlabbatch{1}.spm.spatial.coreg.estwrite.source = pet_image(i);
+matlabbatch{1}.spm.spatial.coreg.estwrite.other = {''};
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'coreg';
 spm_jobman('run',matlabbatch)
 end
+
+
