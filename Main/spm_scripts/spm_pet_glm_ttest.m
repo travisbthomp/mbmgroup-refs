739e96cd7c7ce7d3a882d31ce996fbca66c1305a
Pavan Chaggar <pavanchaggar@pavans-mbp.home>
2020-06-29 20:11:11 +0100

    fixing bugs in script, setting defaults to spm pet

--- Main/spm_scripts/spm_pet_glm_ttest.m
+++ Main/spm_scripts/spm_pet_glm_ttest.m
@@ -8,12 +8,19 @@
 %-----------------------------------------------------------------------
 
 %% Create Subject List
-
+clear all
+spm('Defaults','PET')
 % path to ADNI directory
-data_dir = '/Users/pavanchaggar/Documents/PET/';
+
+group = 'tauPET';
+
+root_dir = '/Volumes/Pavan_SSD/Connectome_atrophy/Data/LMCI/';
+stat_dir = strcat(root_dir, group, '_stats/');
+
 
 % path to ADNI csv file containing subject information
-csv_path = '/Users/pavanchaggar/Documents/PET/AD_CN_PET_TEST_6_02_2020.csv';
+
+csv_path = strcat(stat_dir, 'CN_LMCI.csv');
 
 csv = readtable(csv_path);
 
@@ -23,20 +30,23 @@ subject_ids = csv.Subject;
 subject_groups = csv.Group;
 
 % make path string structure
-group1 = strcat(data_dir, subject_groups, '_', subject_ids, '/', 'norm', subject_ids, '.nii');
+group1 = strcat(root_dir, subject_groups, '/', group, '/', subject_groups, '_', subject_ids, '/', 'ss_suvr_', subject_ids, '.nii');
 
-group2 = strcat(data_dir, subject_groups, '_', subject_ids, '/', 'norm', subject_ids, '.nii');
+group2 = strcat(root_dir, subject_groups, '/', group, '/', subject_groups, '_', subject_ids, '/', 'ss_suvr_', subject_ids, '.nii');
 
 % create masks for groups and edit group arrays
 group1_mask = string(csv.Group) == "CN";
 
-group2_mask = string(csv.Group) == "AD";
+group2_mask = string(csv.Group) == "LMCI";
 
 group1 = group1(group1_mask);
 group2 = group2(group2_mask);
 
 
-matlabbatch{1}.spm.stats.factorial_design.dir = data_dir;
+tissue_vols_path = strcat(root_dir, 'tissue_vols.csv');
+tissue_vols = readtable(tissue_vols_path, 'Delimiter',',');
+
+matlabbatch{1}.spm.stats.factorial_design.dir = {stat_dir};
 matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = group1;
 matlabbatch{1}.spm.stats.factorial_design.des.t2.scans2 = group2;
 matlabbatch{1}.spm.stats.factorial_design.des.t2.dept = 0;
@@ -48,15 +58,15 @@ matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI'
 matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
 matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
 matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
-matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_user.global_uval = tissue_vols{:,2}+tissue_vols{:,3}+tissue_vols{:,4};
 matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
-matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 2;
 matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
 matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
 matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
 matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'group1 v group2';
-matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [1 -1];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'g1<g2';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [-1 1];
 matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
 matlabbatch{3}.spm.stats.con.delete = 0;
 matlabbatch{4}.spm.stats.results.spmmat(1) = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
@@ -69,3 +79,6 @@ matlabbatch{4}.spm.stats.results.conspec.conjunction = 1;
 matlabbatch{4}.spm.stats.results.conspec.mask.none = 1;
 matlabbatch{4}.spm.stats.results.units = 1;
 matlabbatch{4}.spm.stats.results.export{1}.ps = true;
+
+spm_jobman('run',matlabbatch)
+
